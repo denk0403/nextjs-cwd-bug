757b322d06cf40b3da4c822e6d6c50b2d5cad87f
Dennis Kats <denniskats43@gmail.com>
2026-01-11 03:55:16 -0500

    Refactor fake-module-loader to remove resource query handling and streamline fake function exports. Export fakeModules from Client component in page3 for improved module accessibility.

--- fake-module-loader.js
+++ fake-module-loader.js
@@ -3,15 +3,9 @@
  * Import "fake-module-{number}" to get `export const value = {number}`
  */
 module.exports = function (source) {
-	const match = this.resourceQuery.match(/^\?(\d+)$/);
 	let result = "";
 	for (let i = 0; i < 300; i++) {
 		result += `export function fakeFunction${i}() { return ${i}; }\n`;
 	}
-
-	if (match) {
-		result += `export const value = ${match[1]};`;
-	}
-
 	return result;
 };
