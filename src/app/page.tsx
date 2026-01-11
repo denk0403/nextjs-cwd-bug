daab533aaf77e712eaca8efb5004e8c795b0cca2
Dennis Kats <denniskats43@gmail.com>
2026-01-11 03:16:59 -0500

    Add path joining utility function to Page component for improved file path handling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 import { fakeModules } from "@/fakeModules";
+import { join } from "node:path";
 import { Client } from "./Client";
 
 export { fakeModules };
 
 export default function Page() {
 	return <Client />
+}
+
+export function test(a: string, b: string) {
+  console.log(join(process.cwd(), a, b));
 }
