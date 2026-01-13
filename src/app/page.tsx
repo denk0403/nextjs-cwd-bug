4a042af7ee80a9a453c4a845e229d5d85902a484
Dennis Kats <denniskats43@gmail.com>
2026-01-12 20:51:58 -0500

    Remove export of fakeModules from page.tsx to simplify the code and reduce bundle size.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
 import { join } from "node:path";
 
-// artificially inflate the bundle size
-export { fakeModules } from "@/fakeModules";
-
 export default function Page() {
 	return "Hello World";
 }
