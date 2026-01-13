3a9b6f6d2b2f726ae4c2a2f09c06fde25136c9b5
Dennis Kats <denniskats43@gmail.com>
2026-01-12 20:53:20 -0500

    Revert "Remove export of fakeModules from page.tsx to simplify the code and reduce bundle size."
    This reverts commit 4a042af7ee80a9a453c4a845e229d5d85902a484.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import { join } from "node:path";
 
+// artificially inflate the bundle size
+export { fakeModules } from "@/fakeModules";
+
 export default function Page() {
 	return "Hello World";
 }
