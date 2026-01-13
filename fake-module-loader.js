61e2f08624a832d43532fd236799f136acb62f6f
Dennis Kats <denniskats43@gmail.com>
2026-01-13 03:48:14 -0500

    cleanup

--- fake-module-loader.js
+++ fake-module-loader.js
@@ -1,8 +1,5 @@
-/**
- * Webpack loader that generates fake module content based on query parameter.
- * Import "fake-module-{number}" to get `export const value = {number}`
- */
-module.exports = function (source) {
+/** Webpack loader that generates a fake module. */
+module.exports = function () {
 	let result = "";
 	for (let i = 0; i < 300; i++) {
 		result += `export function fakeFunction${i}() { return ${i}; }\n`;
